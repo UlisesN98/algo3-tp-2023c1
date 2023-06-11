92774babf88b3baf85e78bc7d617b15569f3b080
Ulises <unieva@fi.uba.ar>
2023-06-11 15:52:35 -0300

    Merge

--- tpCalendario/src/main/java/Main.java
+++ tpCalendario/src/main/java/Main.java
@@ -5,9 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -16,6 +14,9 @@ import java.io.*;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.time.temporal.TemporalAdjusters;
 
 public class Main extends Application {
@@ -29,15 +30,60 @@ public class Main extends Application {
     @FXML
     private Button anterior;
     @FXML
+    private Button crearEventScene;
+    @FXML
+    private Button crearTareaScene;
+    @FXML
     private ChoiceBox<String> opcionesIntervalo;
 
     @FXML
-    private Button cancelarEvento;
+    private Button cancelarActividad;
     @FXML
     private Button eliminar;
     @FXML
-    private VBox detalleEvento;
+    private VBox detalleActividad;
 
+    // Cosas de crearEvento
+    @FXML
+    private Button crearEvento;
+    @FXML
+    private Button cancelarEvento;
+    @FXML
+    private CheckBox completeDayCheckbox;
+    @FXML
+    private TextField eventTitle;
+    @FXML
+    private TextArea eventDescription;
+    @FXML
+    private TextField fieldFechaInicio;
+    @FXML
+    private TextField fieldFechaFin;
+    @FXML
+    private TextField startTimeEvent;
+    @FXML
+    private TextField endTimeEvent;
+    @FXML
+    private CheckBox dailyRepeatCheck;
+    @FXML
+    private TextField frequencyRepeatDaily;
+    @FXML
+    private TextArea areaAlarmsDates;
+    @FXML
+    private TextArea areaAlarmsTimes;
+
+    // Cosas de crearTarea
+    @FXML
+    private Button cancelarTarea;
+    @FXML
+    private TextField taskTitle;
+    @FXML
+    private TextArea taskDescription;
+    @FXML
+    private TextField fieldFecha;
+    @FXML
+    private TextField endTimeTask;
+    @FXML
+    private Button crearTarea;
 
     private Calendario calendario;
     private LocalDate fechaActual;
@@ -79,6 +125,22 @@ public class Main extends Application {
 
         anterior.setOnAction(event -> retroceder());
         siguiente.setOnAction(event -> avanzar());
+
+        crearEventScene.setOnAction(event -> {
+            try {
+                mostrarVistaCrearEvento();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        crearTareaScene.setOnAction(event -> {
+            try {
+                mostrarVistaCrearTarea();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
 
         opcionesIntervalo.getItems().addAll(Frecuencia.DIARIA.toString(), Frecuencia.SEMANAL.toString(), Frecuencia.MENSUAL.toString());
         opcionesIntervalo.setValue(intervalo.toString());
@@ -254,7 +316,7 @@ public class Main extends Application {
         loader.setController(this);
         AnchorPane vista = loader.load();
 
-        cancelarEvento.setOnAction(event -> {
+        cancelarActividad.setOnAction(event -> {
             try {
                 mostrarVistaActividades();
             } catch (IOException e) {
@@ -306,18 +368,18 @@ public class Main extends Application {
             repeticion.setText("Repeticion ✗");
         }
 
-        detalleEvento.getChildren().add(titulo);
-        detalleEvento.getChildren().add(descripcion);
-        detalleEvento.getChildren().add(diaCompleto);
-        detalleEvento.getChildren().add(inicio);
-        detalleEvento.getChildren().add(fin);
-        detalleEvento.getChildren().add(repeticion);
+        detalleActividad.getChildren().add(titulo);
+        detalleActividad.getChildren().add(descripcion);
+        detalleActividad.getChildren().add(diaCompleto);
+        detalleActividad.getChildren().add(inicio);
+        detalleActividad.getChildren().add(fin);
+        detalleActividad.getChildren().add(repeticion);
 
-        detalleEvento.getChildren().add(new Label("Alarmas"));
+        detalleActividad.getChildren().add(new Label("Alarmas"));
         for (Alarma a: evento.getListaAlarmas()) {
             Label alarma = new Label();
             alarma.setText(String.format("Inicio: %s - Efecto: %s", formatoTiempo(a.getInicio()), a.getEfecto()));
-            detalleEvento.getChildren().add(alarma);
+            detalleActividad.getChildren().add(alarma);
         }
     }
 
@@ -327,7 +389,7 @@ public class Main extends Application {
         loader.setController(this);
         AnchorPane vista = loader.load();
 
-        cancelarEvento.setOnAction(event -> {
+        cancelarActividad.setOnAction(event -> {
             try {
                 mostrarVistaActividades();
             } catch (IOException e) {
@@ -377,19 +439,264 @@ public class Main extends Application {
             completada.setText("Completada: ✗");
         }
 
-        detalleEvento.getChildren().add(titulo);
-        detalleEvento.getChildren().add(descripcion);
-        detalleEvento.getChildren().add(diaCompleto);
-        detalleEvento.getChildren().add(inicio);
-        detalleEvento.getChildren().add(fin);
-        detalleEvento.getChildren().add(completada);
+        detalleActividad.getChildren().add(titulo);
+        detalleActividad.getChildren().add(descripcion);
+        detalleActividad.getChildren().add(diaCompleto);
+        detalleActividad.getChildren().add(inicio);
+        detalleActividad.getChildren().add(fin);
+        detalleActividad.getChildren().add(completada);
 
-        detalleEvento.getChildren().add(new Label("Alarmas"));
+        detalleActividad.getChildren().add(new Label("Alarmas"));
         for (Alarma a: tarea.getListaAlarmas()) {
             Label alarma = new Label();
             alarma.setText(String.format("Inicio: %s - Efecto: %s", formatoTiempo(a.getInicio()), a.getEfecto()));
-            detalleEvento.getChildren().add(alarma);
+            detalleActividad.getChildren().add(alarma);
         }
+    }
+
+    public void mostrarVistaCrearEvento() throws IOException{
+        // Load the FXML file
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("scenaCrearEvento.fxml"));
+        loader.setController(this);
+        AnchorPane vista = loader.load();
+
+        crearEvento.setOnAction(event -> {
+            try {
+                crearEvento();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        cancelarEvento.setOnAction(event -> {
+            try {
+                mostrarVistaActividades();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        // Create the scene
+        Scene scene = new Scene(vista);
+        escenario.setScene(scene);
+        escenario.show();
+    }
+
+    public void mostrarVistaCrearTarea() throws IOException{
+        // Load FXML file
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ScenaCrearTarea.fxml"));
+        loader.setController(this);
+        AnchorPane vista = loader.load();
+
+        crearTarea.setOnAction(event -> {
+            try {
+                crearTarea();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        cancelarTarea.setOnAction(event -> {
+            try {
+                mostrarVistaActividades();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        // Create the scene
+        Scene scene = new Scene(vista);
+        escenario.setScene(scene);
+        escenario.show();
+    }
+
+    // METODOS RELATIVOS A LA CREACION DE EVENTOS Y TAREAS
+
+    public void crearEvento() throws IOException {
+        // Validar input del usuario.
+        if (!esFechaValida(fieldFechaInicio.getText())) {
+            showErrorAlert("Formato de fecha de inicio invalido");
+            return;
+        }
+
+        if (!esFechaValida(fieldFechaFin.getText())) {
+            showErrorAlert("Formato de fecha de finalizacion invalido");
+            return;
+        }
+
+        if (!esTiempoValido(startTimeEvent.getText())) {
+            showErrorAlert("Formato de horario de inicio invalido");
+            return;
+        }
+
+        if (!esTiempoValido(endTimeEvent.getText())) {
+            showErrorAlert("Formato de horario de finalizacion invalido");
+            return;
+        }
+
+        if (dailyRepeatCheck.isSelected()) {
+            if (!frequencyRepeatDaily.getText().matches("[1-9]\\d*")){
+                showErrorAlert("Formato de frequencia de repeticion diaria invalido");
+                return;
+            }
+        }
+
+        String[] alarmDates = areaAlarmsDates.getText().isEmpty() ? new String[0] : areaAlarmsDates.getText().split(" ");
+        String[] alarmTimes = areaAlarmsTimes.getText().isEmpty() ? new String[0] : areaAlarmsTimes.getText().split(" ");
+
+
+        if (alarmDates.length != alarmTimes.length) {
+            showErrorAlert("La cantidad de fechas de alarmas no coincide con la cantidad de horarios de alarma");
+            return;
+        }
+
+
+        for (String alarmDate : alarmDates) {
+            if (!esFechaValida(alarmDate)) {
+                showErrorAlert("Formato de fecha invalido para alguna/s de la/s fecha/s de la/s alarma/s");
+                return;
+            }
+        }
+
+        for (String alarmTime : alarmTimes) {
+            if (!esTiempoValido(alarmTime)) {
+                showErrorAlert("Formato de tiempo invalido para alguno/s de lo/s tiempo/s de la/s alarma/s");
+                return;
+            }
+        }
+
+        String fechaInicio = fieldFechaInicio.getText();
+        String fechaFin = fieldFechaFin.getText();
+
+        String tituloEvento = eventTitle.getText();
+        String descripcionEvento = eventDescription.getText();
+
+        String inicioTiempoEvento = startTimeEvent.getText();
+        String finTiempoEvento = endTimeEvent.getText();
+
+        boolean diaCompleto = completeDayCheckbox.isSelected();
+        boolean seRepite = dailyRepeatCheck.isSelected();
+
+
+        // Una vez validados los inputs, realizar creacion de evento.
+
+        LocalDate fechaInicioFormatProcess = LocalDate.parse(fechaInicio, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        LocalTime tiempoInicioFormatProcess = LocalTime.parse(inicioTiempoEvento, DateTimeFormatter.ofPattern("HH:mm"));
+        LocalDateTime inicioEventoFormateado = fechaInicioFormatProcess.atTime(tiempoInicioFormatProcess);
+
+        LocalDate fechaFinalFormatProcess = LocalDate.parse(fechaFin, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        LocalTime tiempoFinalFormatProcess = LocalTime.parse(finTiempoEvento, DateTimeFormatter.ofPattern("HH:mm"));
+        LocalDateTime finEventoFormateado = fechaFinalFormatProcess.atTime(tiempoFinalFormatProcess);
+
+        LocalDateTime[] alarmasFormateadas = new LocalDateTime[alarmDates.length];
+
+        for (int i = 0; i < alarmDates.length; i++) {
+            String date = alarmDates[i];
+            String time = alarmTimes[i];
+
+            LocalDateTime fechaTiempoFormateado = LocalDateTime.parse(date + "T" + time, DateTimeFormatter.ofPattern("dd/MM/yyyy'T'HH:mm"));
+            alarmasFormateadas[i] = fechaTiempoFormateado;
+        }
+
+        if (seRepite){
+            Integer frecuenciaRepeticion = Integer.valueOf(frequencyRepeatDaily.getText());
+            Repeticion repeticion = new RepeticionDiariaIntervalo(inicioEventoFormateado, frecuenciaRepeticion);
+            calendario.crearEvento(tituloEvento, descripcionEvento, diaCompleto, inicioEventoFormateado, finEventoFormateado, alarmasFormateadas, new Efecto[]{Efecto.NOTIFICACION}, repeticion);
+            guardarEstado();
+        }
+
+        else {
+            calendario.crearEvento(tituloEvento, descripcionEvento, diaCompleto, inicioEventoFormateado, finEventoFormateado, alarmasFormateadas, new Efecto[]{Efecto.NOTIFICACION}, null);
+            guardarEstado();
+        }
+        mostrarVistaActividades();
+    }
+
+    public void crearTarea() throws IOException{
+        // Validar input del usuario
+        if (!esFechaValida(fieldFecha.getText())){
+            showErrorAlert("Formato de fecha invalido");
+            return;
+        }
+
+        if (!esTiempoValido(endTimeTask.getText())){
+            showErrorAlert("Formato de horario limite invalido");
+            return;
+        }
+
+        String[] alarmDates = areaAlarmsDates.getText().isEmpty() ? new String[0] : areaAlarmsDates.getText().split(" ");
+        String[] alarmTimes = areaAlarmsTimes.getText().isEmpty() ? new String[0] : areaAlarmsTimes.getText().split(" ");
+
+
+        if (alarmDates.length != alarmTimes.length) {
+            showErrorAlert("La cantidad de fechas de alarmas no coincide con la cantidad de horarios de alarma");
+            return;
+        }
+
+
+        for (String alarmDate : alarmDates) {
+            if (!esFechaValida(alarmDate)) {
+                showErrorAlert("Formato de fecha invalido para alguna/s de la/s fecha/s de la/s alarma/s");
+                return;
+            }
+        }
+
+        for (String alarmTime : alarmTimes) {
+            if (!esTiempoValido(alarmTime)) {
+                showErrorAlert("Formato de tiempo invalido para alguno/s de lo/s tiempo/s de la/s alarma/s");
+                return;
+            }
+        }
+
+        String fechaInicio = fieldFecha.getText();
+
+        String tituloTarea = taskTitle.getText();
+        String descripcionTarea = taskDescription.getText();
+
+        String limiteTarea = endTimeTask.getText();
+
+        boolean diaCompleto = completeDayCheckbox.isSelected();
+
+        // Una vez validados los inputs, realizar creacion de evento.
+
+        LocalDate fechaInicioFormatProcess = LocalDate.parse(fechaInicio, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        LocalTime tiempoInicioFormatProcess = LocalTime.parse(limiteTarea, DateTimeFormatter.ofPattern("HH:mm"));
+        LocalDateTime dateTimeTareaFormateado = fechaInicioFormatProcess.atTime(tiempoInicioFormatProcess);
+
+        LocalDateTime[] alarmasFormateadas = new LocalDateTime[alarmDates.length];
+
+        for (int i = 0; i < alarmDates.length; i++) {
+            String date = alarmDates[i];
+            String time = alarmTimes[i];
+
+            LocalDateTime fechaTiempoFormateado = LocalDateTime.parse(date + "T" + time, DateTimeFormatter.ofPattern("dd/MM/yyyy'T'HH:mm"));
+            alarmasFormateadas[i] = fechaTiempoFormateado;
+        }
+
+        calendario.crearTarea(tituloTarea, descripcionTarea, diaCompleto, dateTimeTareaFormateado, alarmasFormateadas, new Efecto[]{Efecto.NOTIFICACION});
+        guardarEstado();
+        mostrarVistaActividades();
+    }
+
+    public void showErrorAlert(String mensaje){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Informacion");
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
+    public boolean esFechaValida(String date) {
+        try {
+            LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+    }
+
+    public boolean esTiempoValido(String time) {
+        return time.matches("(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]");
     }
 
     // METODOS PARA ELIMINAR
